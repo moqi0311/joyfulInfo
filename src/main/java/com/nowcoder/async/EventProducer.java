e1c9989abff006216c69640c141f7819ee9f90d3
moqi0311 <503594873@qq.com>
2020-06-03 08:19:01 +0800

    the version 1.0

---
+++ src/main/java/com/nowcoder/async/EventProducer.java
@@ -0,0 +1,27 @@
+package com.nowcoder.async;
+
+import com.alibaba.fastjson.JSONObject;
+import com.nowcoder.util.JedisAdapter;
+import com.nowcoder.util.RedisKeyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by nowcoder on 2016/7/16.
+ */
+@Service
+public class EventProducer {
+    @Autowired
+    JedisAdapter jedisAdapter;
+
+    public boolean fireEvent(EventModel model) {
+        try {
+            String json = JSONObject.toJSONString(model);
+            String key = RedisKeyUtil.getEventQueueKey();
+            jedisAdapter.lpush(key, json);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/nowcoder/async/EventType.java
@@ -0,0 +1,21 @@
+package com.nowcoder.async;
+
+/**
+ * Created by nowcoder on 2016/7/16.
+ */
+public enum EventType {
+    LIKE(0),
+    COMMENT(1),
+    LOGIN(2),
+    MAIL(3);
+
+    private int value;
+
+    EventType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
